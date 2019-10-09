f4377a06499b6606f5902230707095c523baa039
Lucas Penteado <38321808+lucaspsacchi@users.noreply.github.com>
2019-10-09 16:11:29 -0300

    Alteração nos padrões das páginas

---
+++ js/dadosCateg.js
@@ -0,0 +1,40 @@
+const dados = [{
+  id_video: 'IAZYoNs7kU4',
+  nome: 'Curtir uma Página',
+  categ: 'Facebook'
+},
+{
+  id_video: 'K1xfGs7pGho',
+  nome: 'Compartilhar uma Publicação',
+  categ: 'Facebook'
+},
+{
+  id_video: 'cbhTDynLA74',
+  nome: 'Trocar Foto de Perfil',
+  categ: 'Facebook'
+},
+{
+  id_video: 'rukpFI0pLZ4',
+  nome: 'Chamada de Voz no WhatsApp',
+  categ: 'WhatsApp'
+},
+{
+  id_video: 'RlJk9Mjpcv0',
+  nome: 'Responder Mensagem Específica',
+  categ: 'WhatsApp'
+},
+{
+  id_video: 'L10CJs6pKI4',
+  nome: 'Deletar Mensagem em Conversa',
+  categ: 'WhatsApp'
+},
+{
+  id_video: '7UsZo4wzVZU',
+  nome: 'Curtir uma Publicação',
+  categ: 'Instagram'
+},
+{
+  id_video: 'mUGJkqYFbYA',
+  nome: 'Seguir uma Conta',
+  categ: 'Instagram'
+}]
---
+++ js/dadosHome.js
@@ -0,0 +1,18 @@
+const dados = [{
+  categoria: 'WhatsApp',
+  total: 10,
+  completado: 10,
+  img: 'whatsapp.png'
+},
+{
+  categoria: 'Facebook',
+  total: 10,
+  completado: 5,
+  img: 'facebook.png'
+},
+{
+  categoria: 'Instagram',
+  total: 8,
+  completado: 3,
+  img: 'instagram.jpeg'
+}]
