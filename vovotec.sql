e95bc9c1cf718ed18e281f3fe0d514121e133874
Lucas Penteado <38321808+lucaspsacchi@users.noreply.github.com>
2019-10-20 21:39:50 -0300

    Tabela formatada e dados inseridos dos dados.js

--- vovotec.sql
+++ vovotec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 20-Out-2019 às 23:36
+-- Generation Time: 21-Out-2019 às 00:38
 -- Versão do servidor: 5.6.37
 -- PHP Version: 5.6.31
 
@@ -31,14 +31,16 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `categoria` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `img` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `categoria`
 --
 
 INSERT INTO `categoria` (`id`, `categoria`, `alias`, `img`) VALUES
-(2, 'Facebook', 'fb', 'facebook.jpg');
+(2, 'Facebook', 'fb,face', 'facebook.png'),
+(3, 'WhatsApp', 'wpp,zapzap', 'whatsapp.png'),
+(4, 'Instagram', 'insta', 'instagram.jpeg');
 
 -- --------------------------------------------------------
 
@@ -50,14 +52,25 @@ CREATE TABLE IF NOT EXISTS `modelo_alternativa` (
   `id` int(11) NOT NULL,
   `id_pergunta` int(11) NOT NULL,
   `img1` varchar(255) NOT NULL,
-  `resposta1` varchar(255) NOT NULL,
+  `resposta1` tinyint(1) NOT NULL,
   `img2` varchar(255) NOT NULL,
-  `resposta2` varchar(255) NOT NULL,
+  `resposta2` tinyint(1) NOT NULL,
   `img3` varchar(255) NOT NULL,
-  `resposta3` varchar(255) NOT NULL,
+  `resposta3` tinyint(1) NOT NULL,
   `img4` varchar(255) NOT NULL,
-  `resposta4` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `resposta4` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `modelo_alternativa`
+--
+
+INSERT INTO `modelo_alternativa` (`id`, `id_pergunta`, `img1`, `resposta1`, `img2`, `resposta2`, `img3`, `resposta3`, `img4`, `resposta4`) VALUES
+(1, 3, './img/facebook3pts.jpg', 0, './img/facebookBuscar.png', 0, './img/facebookCompartilhar.jpg', 0, './img/facebookCurtir.jpg', 1),
+(2, 5, './img/facebookCompartilhar.jpg', 1, './img/facebookComentar.jpg', 0, './img/facebookCurtir.jpg', 0, './img/facebook3pts.jpg', 0),
+(3, 6, './img/instagram.jpeg', 0, './img/facebookRedimensionado.png', 1, './img/playstoreRedimensionado.png', 0, './img/whatsappRedimensionado.png', 0),
+(4, 9, './img/facebookRedimensionado.png', 0, './img/instagram.jpeg', 0, './img/whatsappRedimensionado.png', 1, './img/playstoreRedimensionado.png', 0),
+(5, 10, './img/setaCimaBaixo.png', 0, './img/setaDirEsq.png', 0, './img/setaEsqDir.png', 1, './img/setaBaixoCima.png', 0);
 
 -- --------------------------------------------------------
 
@@ -70,7 +83,20 @@ CREATE TABLE IF NOT EXISTS `modelo_pares` (
   `id_pergunta` int(11) NOT NULL,
   `texto` varchar(255) NOT NULL,
   `resposta` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `modelo_pares`
+--
+
+INSERT INTO `modelo_pares` (`id`, `id_pergunta`, `texto`, `resposta`) VALUES
+(1, 12, 'Status', 'Foto/vídeo disponível por 24h'),
+(2, 12, 'Confirmação de leitura', 'Ícone que mostra se já leu a conversa'),
+(5, 12, 'Localização em tempo real', 'Posição durante um período de tempo'),
+(6, 12, 'Contatos bloqueados', 'Pessoas que não podem mandar mensagem/ligar'),
+(7, 13, 'Todos', 'Qualquer pessoa, mesmo quem não está nos seus contatos'),
+(8, 13, 'Meus Contatos', 'Apenas pessoas que você tem o número salvo'),
+(9, 13, 'Ninguém', 'Nenhuma pessoa, mesmo quem está nos seus contantos');
 
 -- --------------------------------------------------------
 
@@ -83,15 +109,39 @@ CREATE TABLE IF NOT EXISTS `modelo_sequencia` (
   `id_pergunta` int(11) NOT NULL,
   `texto` varchar(255) NOT NULL,
   `posicao` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `modelo_sequencia`
 --
 
 INSERT INTO `modelo_sequencia` (`id`, `id_pergunta`, `texto`, `posicao`) VALUES
-(1, 1, 'Home', 1),
-(2, 1, 'Fechar', 2);
+(3, 2, 'Selecionar lupa', 1),
+(4, 2, 'Digitar nome', 2),
+(5, 2, 'Buscar', 3),
+(6, 2, 'Selecionar página', 4),
+(7, 2, 'Curtir', 5),
+(8, 4, 'Escolher publicação', 1),
+(9, 4, 'Compartilhar', 2),
+(10, 4, 'Escrever mensagem', 3),
+(11, 4, 'Compartilhar agora', 4),
+(12, 8, 'Localizar ícones no canto superior da tela', 1),
+(13, 8, 'Telefone', 2),
+(14, 8, 'Conversar com pessoa', 3),
+(15, 8, 'Botão vermelho', 4),
+(16, 11, 'Três pontinhos', 1),
+(17, 11, 'Configurações', 2),
+(18, 11, 'Conta', 3),
+(19, 11, 'Privacidade', 4),
+(20, 14, 'Página inicial', 1),
+(21, 14, 'Encontrar publicação', 2),
+(22, 14, 'Clicar duas vezes na foto', 3),
+(23, 14, 'Verificar coração vermelho', 4),
+(24, 15, 'Clicar na lupa', 1),
+(25, 15, 'Clicar na barra de busca', 2),
+(26, 15, 'Digitar nome da página', 3),
+(27, 15, 'Selecionar uma página', 4),
+(28, 15, 'Seguir', 5);
 
 -- --------------------------------------------------------
 
@@ -101,18 +151,30 @@ INSERT INTO `modelo_sequencia` (`id`, `id_pergunta`, `texto`, `posicao`) VALUES
 
 CREATE TABLE IF NOT EXISTS `pergunta` (
   `id` int(11) NOT NULL,
-  `id_video` int(11) NOT NULL,
-  `id_cat` int(11) NOT NULL,
+  `id_video` varchar(11) NOT NULL,
   `modelo` varchar(255) NOT NULL,
   `pergunta` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `pergunta`
 --
 
-INSERT INTO `pergunta` (`id`, `id_video`, `id_cat`, `modelo`, `pergunta`) VALUES
-(1, 7, 2, 'sequencia', 'Selecione a sequencia');
+INSERT INTO `pergunta` (`id`, `id_video`, `modelo`, `pergunta`) VALUES
+(2, 'IAZYoNs7kU4', 'sequencia', 'Selecione a sequência de passos para curtir uma página:'),
+(3, 'IAZYoNs7kU4', 'alternativa', 'Selecione o ícone que representa a ação de curtir uma página:'),
+(4, 'K1xfGs7pGho', 'sequencia', 'Selecione a sequência de passos para compartilhar uma publicação'),
+(5, 'K1xfGs7pGho', 'alternativa', 'Selecione o ícone que representa "Compartilhar":'),
+(6, 'cbhTDynLA74', 'alternativa', 'Selecione o ícone do Facebook:'),
+(7, 'cbhTDynLA74', 'sequencia', 'Selecione a sequência de passos para alterar a foto de perfil'),
+(8, 'rukpFI0pLZ4', 'sequencia', 'Selecione a sequência de passos para realizar uma chamada de voz'),
+(9, 'RlJk9Mjpcv0', 'alternativa', 'Selecione o ícone do WhatsApp:'),
+(10, 'RlJk9Mjpcv0', 'alternativa', 'Selecione o movimento que deve ser realizado para responder uma mensagem específica:'),
+(11, 'L10CJs6pKI4', 'sequencia', 'Selecione a sequência de passos para chegar nas configurações de privacidade:'),
+(12, '63jDpRxhGsI', 'pares', 'Toque nos pares de configurações e seus significados:'),
+(13, '63jDpRxhGsI', 'pares', 'Toque nos pares de tipos de privacidade e seus significados'),
+(14, '7UsZo4wzVZU', 'sequencia', 'Selecione a sequência de passos para curtir uma publicação:'),
+(15, 'mUGJkqYFbYA', 'sequencia', 'Selecione a sequência de passos para seguir uma página');
 
 -- --------------------------------------------------------
 
@@ -123,7 +185,7 @@ INSERT INTO `pergunta` (`id`, `id_video`, `id_cat`, `modelo`, `pergunta`) VALUES
 CREATE TABLE IF NOT EXISTS `pergunta_pessoa` (
   `id_pergunta` int(11) NOT NULL,
   `id_pessoa` int(11) NOT NULL,
-  `id_video` int(11) NOT NULL,
+  `id_video` varchar(11) NOT NULL,
   `id_cat` int(11) NOT NULL,
   `flag` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -155,17 +217,25 @@ INSERT INTO `pessoa` (`id`, `nome`, `conhecimento`, `flag`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `video_aula` (
-  `id_video` int(11) NOT NULL,
+  `id_video` varchar(11) NOT NULL,
   `id_cat` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `video_aula`
 --
 
 INSERT INTO `video_aula` (`id_video`, `id_cat`, `nome`) VALUES
-(7, 2, 'Como mexer no Facebook');
+('63jDpRxhGsI', 4, 'Alterar Configurações de Privacidade'),
+('7UsZo4wzVZU', 4, 'Curtir uma Publicação'),
+('cbhTDynLA74', 2, 'Trocar Foto de Perfil'),
+('IAZYoNs7kU4', 2, 'Curtir uma Página'),
+('K1xfGs7pGho', 2, 'Compartilhar uma Publicação'),
+('L10CJs6pKI4', 3, 'Deletar Mensagem em Conversa'),
+('mUGJkqYFbYA', 4, 'Seguir uma Conta'),
+('RlJk9Mjpcv0', 3, 'Responder Mensagem Específica'),
+('rukpFI0pLZ4', 3, 'Chamada de Voz no WhatsApp');
 
 --
 -- Indexes for dumped tables
@@ -204,9 +274,7 @@ ALTER TABLE `modelo_sequencia`
 --
 ALTER TABLE `pergunta`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `fk_va_to_pergunta` (`id_video`),
-  ADD KEY `fk_cat_to_pergunta` (`id_cat`) USING BTREE,
-  ADD KEY `id_video` (`id_video`,`id_cat`);
+  ADD KEY `fk_va_to_pergunta` (`id_video`);
 
 --
 -- Indexes for table `pergunta_pessoa`
@@ -239,27 +307,27 @@ ALTER TABLE `video_aula`
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `modelo_alternativa`
 --
 ALTER TABLE `modelo_alternativa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `modelo_pares`
 --
 ALTER TABLE `modelo_pares`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `modelo_sequencia`
 --
 ALTER TABLE `modelo_sequencia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT for table `pergunta`
 --
 ALTER TABLE `pergunta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `pergunta_pessoa`
 --
@@ -270,11 +338,6 @@ ALTER TABLE `pergunta_pessoa`
 --
 ALTER TABLE `pessoa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `video_aula`
---
-ALTER TABLE `video_aula`
-  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- Constraints for dumped tables
 --
@@ -301,16 +364,15 @@ ALTER TABLE `modelo_sequencia`
 -- Limitadores para a tabela `pergunta`
 --
 ALTER TABLE `pergunta`
-  ADD CONSTRAINT `pergunta_ibfk_1` FOREIGN KEY (`id_video`) REFERENCES `video_aula` (`id_video`),
-  ADD CONSTRAINT `pergunta_ibfk_2` FOREIGN KEY (`id_cat`) REFERENCES `categoria` (`id`);
+  ADD CONSTRAINT `pergunta_ibfk_3` FOREIGN KEY (`id_video`) REFERENCES `video_aula` (`id_video`);
 
 --
 -- Limitadores para a tabela `pergunta_pessoa`
 --
 ALTER TABLE `pergunta_pessoa`
   ADD CONSTRAINT `pergunta_pessoa_ibfk_1` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`),
-  ADD CONSTRAINT `pergunta_pessoa_ibfk_2` FOREIGN KEY (`id_video`) REFERENCES `video_aula` (`id_video`),
-  ADD CONSTRAINT `pergunta_pessoa_ibfk_3` FOREIGN KEY (`id_cat`) REFERENCES `categoria` (`id`);
+  ADD CONSTRAINT `pergunta_pessoa_ibfk_3` FOREIGN KEY (`id_cat`) REFERENCES `categoria` (`id`),
+  ADD CONSTRAINT `pergunta_pessoa_ibfk_4` FOREIGN KEY (`id_video`) REFERENCES `video_aula` (`id_video`);
 
 --
 -- Limitadores para a tabela `video_aula`
